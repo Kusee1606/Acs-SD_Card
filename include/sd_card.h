702bad75805015ccded85865fad25bd39a530b04
Kusee1606 <78727649+Kusee1606@users.noreply.github.com>
2024-12-16 19:54:33 +0100

    changed init function

--- include/sd_card.h
+++ include/sd_card.h
@@ -4,9 +4,23 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#include "flight_data.h"
+#include "../include/flight_data.h"
 
-bool init_sd_card();
-void update_sd_card_data();
+typedef struct
+{
+    esp_vfs_fat_sdmmc_mount_config_t mount_config;
+    sdspi_device_config_t slot_config;
+    spi_bus_config_t bus_cfg;
+    sdmmc_card_t *card;
+    sdmmc_host_t host;
+    char path[256];
+    FILE *log_file;
+} sd_card_t;
+
+bool init_sd_card(sd_card_t *sd_card_data);
+bool mount_sd_card(sd_card_t *sd_card_data);
+bool unmount_sd_card(sd_card_t *sd_card_data);
+bool find_free_path(sd_card_t *sd_card_data);
+void update_sd_card_data(sd_card_t *sd_card_data);
 
 #endif //__SDCARD__
--- main/sd_card.c
+++ main/sd_card.c
@@ -11,84 +11,103 @@
 #include "string.h"
 #include "sys/stat.h"
 
-
 #define TAG "SD_CARD"
 #define MOUNT_POINT "/sdcard"
-#define FILENAME "/LOT_%d.csv"
+#define FILENAME "LOT_"
 #define PIN_NUM_MISO 19
 #define PIN_NUM_MOSI 23
 #define PIN_NUM_CLK 18
 #define PIN_NUM_CS 5
+#define SPI_FREQUENCY 5000
 
-FILE *file;
-
-bool init_sd_card()
+bool init_sd_card(sd_card_t *sd_card_data)
 {
+    esp_err_t ret;
+
+    ESP_LOGI(TAG, "Initializing SD card");
+
+    // Pin configuration
     gpio_set_pull_mode(PIN_NUM_MISO, GPIO_PULLUP_ONLY);
     gpio_set_pull_mode(PIN_NUM_MOSI, GPIO_PULLUP_ONLY);
     gpio_set_pull_mode(PIN_NUM_CLK, GPIO_PULLUP_ONLY);
     gpio_set_pull_mode(PIN_NUM_CS, GPIO_PULLUP_ONLY);
-    bool error_occurred = false;
-    ESP_LOGI(TAG, "Initializing SD card");
-    esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+
+    esp_vfs_fat_sdmmc_mount_config_t mount_config_init = {
         .format_if_mount_failed = false,
         .max_files = 2,
         .allocation_unit_size = 16 * 1024};
-    sdmmc_card_t *card;
-    sdmmc_host_t host = SDSPI_HOST_DEFAULT();
-    host.max_freq_khz = 5000;
-    spi_bus_config_t bus_cfg = {
+
+    sdmmc_card_t *card_init;
+
+    sdmmc_host_t host_init = SDSPI_HOST_DEFAULT();
+    host_init.max_freq_khz = SPI_FREQUENCY;
+
+    spi_bus_config_t bus_cfg_init = {
         .mosi_io_num = PIN_NUM_MOSI,
         .miso_io_num = PIN_NUM_MISO,
         .sclk_io_num = PIN_NUM_CLK,
         .quadwp_io_num = -1,
         .quadhd_io_num = -1,
-        .max_transfer_sz = 4000,
-    };
-    ESP_LOGI(TAG, "Initializing SPI device");
-    error_occurred |= (ESP_ERROR_CHECK_WITHOUT_ABORT(spi_bus_initialize(host.slot, &bus_cfg, SDSPI_DEFAULT_DMA)) != ESP_OK);
-    sdspi_device_config_t slot_config = SDSPI_DEVICE_CONFIG_DEFAULT();
-    slot_config.gpio_cs = PIN_NUM_CS;
-    slot_config.host_id = host.slot;
+        .max_transfer_sz = 0};
+
+    sdspi_device_config_t slot_config_init = SDSPI_DEVICE_CONFIG_DEFAULT();
+    slot_config_init.gpio_cs = PIN_NUM_CS;
+    slot_config_init.host_id = sd_card_data->host.slot;
+
+    sd_card_data->mount_config = mount_config_init;
+    sd_card_data->slot_config = slot_config_init;
+    sd_card_data->card = card_init;
+    sd_card_data->host = host_init;
+    sd_card_data->bus_cfg = bus_cfg_init;
+
+    ESP_LOGI(TAG, "Initializing SPI bus");
+    ret = ESP_ERROR_CHECK_WITHOUT_ABORT(spi_bus_initialize(sd_card_data->host.slot, &sd_card_data->bus_cfg, SDSPI_DEFAULT_DMA));
+    if (ret == ESP_OK)
+        return false;
+
     ESP_LOGI(TAG, "Mounting SD card to %s ", MOUNT_POINT);
-    error_occurred |= (ESP_ERROR_CHECK_WITHOUT_ABORT(esp_vfs_fat_sdspi_mount(MOUNT_POINT, &host, &slot_config, &mount_config, &card)) != ESP_OK);
-    sdmmc_card_print_info(stdout, card);
+    if (!mount_sd_card(sd_card_data))
+        return false;
+    sdmmc_card_print_info(stdout, sd_card_data->card);
 
-    char *path = malloc(strlen(MOUNT_POINT) + strlen(FILENAME) + 1);
-    int path_size = strlen(path) + strlen("1000");
-    char *formated_path = malloc(path_size);
-
-    strcpy(path, MOUNT_POINT);
-    strcat(path, FILENAME);
-    sprintf(formated_path, path, 1);
-    struct stat fdata;
-    int i = 1;
-    while (stat(formated_path, &fdata) == 0)
-    {
-        i++;
-        if (i > 100)
-        {
-            ESP_LOGE(TAG, "Clean SD Card");
-            error_occurred |= 1;
-            return !(error_occurred);
-        }
-        sprintf(formated_path, path, i);
-    }
-    ESP_LOGI(TAG, "Trying to create file %s", formated_path);
-    file = fopen(formated_path, "a+");
-    if (file == NULL)
+    if (!find_free_path(sd_card_data))
+        return false;
+    ESP_LOGI(TAG, "Creating a file %s", sd_card_data->path);
+    sd_card_data->log_file = fopen(sd_card_data->path, "a+");
+    if (sd_card_data->log_file == NULL)
     {
         ESP_LOGE(TAG, "Falied to create file");
-        error_occurred |= (file != NULL);
-        return !(error_occurred);
+        return false;
     }
     else
         ESP_LOGI(TAG, "File created");
 
-    fclose(file);
-    return !(error_occurred);
+    fclose(sd_card_data->log_file);
+    return true;
 }
 
-void update_sd_card_data()
+bool mount_sd_card(sd_card_t *sd_card_data)
 {
+    esp_err_t ret;
+    ret = ESP_ERROR_CHECK_WITHOUT_ABORT(esp_vfs_fat_sdspi_mount(MOUNT_POINT, &sd_card_data->host, &sd_card_data->slot_config, &sd_card_data->mount_config, &sd_card_data->card));
+    if (ret = ESP_OK)
+        return false;
+    return true;
+}
+
+bool find_free_path(sd_card_t *sd_card_data)
+{
+    char path_init[256];
+    struct stat fdata;
+    for (int i = 0; i < 1000; i++)
+    {
+        snprintf(path_init, 256, "%s/%s%d.csv", MOUNT_POINT, FILENAME, i);
+        if (stat(path_init, &fdata) != 0)
+        {
+            memcpy(sd_card_data->path, path_init, 256);
+            return true;
+        }
+    }
+    ESP_LOGE(TAG, "Unable to find a path");
+    return false;
 }
